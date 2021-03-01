a72d085c1c8bdc24e3d16bcf8b8c7d7e835976b1
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-03-01 21:34:46 +0100

    Add credentials

--- app.js
+++ app.js
@@ -61,7 +61,11 @@ app.use(compression());
 app.use(helmet());
 app.disable("x-powered-by");
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
